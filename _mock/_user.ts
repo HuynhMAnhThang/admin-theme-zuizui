1817a018ae74d52acc047f3405ef343b18185489
ThangHM <HuynhMAnhThang>
2021-05-18 00:27:54 +0700

    thanghm

---
+++ _mock/_user.ts
@@ -0,0 +1,122 @@
+import { MockRequest } from '@delon/mock';
+
+const list: any[] = [];
+const total = 50;
+
+for (let i = 0; i < total; i += 1) {
+  list.push({
+    id: i + 1,
+    disabled: i % 6 === 0,
+    href: 'https://ant.design',
+    avatar: [
+      'https://gw.alipayobjects.com/zos/rmsportal/eeHMaZBwmTvLdIwMfBpg.png',
+      'https://gw.alipayobjects.com/zos/rmsportal/udxAbMEhpwthVVcjLXik.png',
+    ][i % 2],
+    no: `TradeCode ${i}`,
+    title: `一个任务名称 ${i}`,
+    owner: '曲丽丽',
+    description: '这是一段描述',
+    callNo: Math.floor(Math.random() * 1000),
+    status: Math.floor(Math.random() * 10) % 4,
+    updatedAt: new Date(`2017-07-${Math.floor(i / 2) + 1}`),
+    createdAt: new Date(`2017-07-${Math.floor(i / 2) + 1}`),
+    progress: Math.ceil(Math.random() * 100),
+  });
+}
+
+function genData(params: any): { total: number; list: any[] } {
+  let ret = [...list];
+  const pi = +params.pi;
+  const ps = +params.ps;
+  const start = (pi - 1) * ps;
+
+  if (params.no) {
+    ret = ret.filter((data) => data.no.indexOf(params.no) > -1);
+  }
+
+  return { total: ret.length, list: ret.slice(start, ps * pi) };
+}
+
+function saveData(id: number, value: any): { msg: string } {
+  const item = list.find((w) => w.id === id);
+  if (!item) {
+    return { msg: '无效用户信息' };
+  }
+  Object.assign(item, value);
+  return { msg: 'ok' };
+}
+
+export const USERS = {
+  '/user': (req: MockRequest) => genData(req.queryString),
+  '/user/:id': (req: MockRequest) => list.find((w) => w.id === +req.params.id),
+  'POST /user/:id': (req: MockRequest) => saveData(+req.params.id, req.body),
+  '/user/current': {
+    name: 'Huỳnh Mạnh Thắng',
+    avatar: 'https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png',
+    userid: '00000001',
+    email: 'thanghm6368.contact@gmail.com',
+    signature: 'Hãy khoan dung với sự đa dạng, khoan dung là một đức tính',
+    title: 'Chuyên gia tương tác',
+    group: 'Cao đẳng Thực hành FPT Polytechnic',
+    tags: [
+      {
+        key: '0',
+        label: 'Rất chu đáo',
+      },
+      {
+        key: '1',
+        label: 'Tập trung',
+      },
+      {
+        key: '2',
+        label: 'Đẹp trai',
+      },
+      {
+        key: '3',
+        label: 'Bao dung',
+      },
+      {
+        key: '4',
+        label: 'Chương trình phụ trợ toàn thời gian',
+      },
+      {
+        key: '5',
+        label: 'Tất tàn tật về tôi',
+      },
+    ],
+    notifyCount: 12,
+    country: 'China',
+    geographic: {
+      province: {
+        label: 'HÀ NỘI',
+        key: '330000',
+      },
+      city: {
+        label: 'TP HÀ NỘI',
+        key: '330100',
+      },
+    },
+    address: 'Hà Nội',
+    phone: '你猜-你猜你猜猜猜',
+  },
+  'POST /user/avatar': 'ok',
+  'POST /login/account': (req: MockRequest) => {
+    const data = req.body;
+    if (!(data.userName === 'admin' || data.userName === 'user') || data.password !== 'ng-alain.com') {
+      return { msg: `Invalid username or password（admin/ng-alain.com）` };
+    }
+    return {
+      msg: 'ok',
+      user: {
+        token: '123456789',
+        name: data.userName,
+        email: `${data.userName}@qq.com`,
+        id: 10000,
+        time: +new Date(),
+      },
+    };
+  },
+  'POST /register': {
+    msg: 'ok',
+  },
+};
